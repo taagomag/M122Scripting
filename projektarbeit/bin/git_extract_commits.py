c14bd255cd99f31eb8c2015ce2012da9d9c57ec6
taagomag <max.gordonsulzberger1@swisscom.com>
2022-07-08 13:22:53 +0200

    add commentary workflow to py file

--- projektarbeit/bin/git_extract_commits.py
+++ projektarbeit/bin/git_extract_commits.py
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-#Hier kommt ihr code
-
+#read the name of base dir with $1
+#throw error if no base dir was specified or no base dir existierendes
+#read outputfile name 
+#read what in config file was defined in config
+#loop through base dir and check each if there is any data in git logs, if not skip repo
+#write each log collection to output file with defined name
